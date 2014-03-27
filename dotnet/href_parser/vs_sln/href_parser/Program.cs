425a81721e7d9dad1e4d1fb33e5fa9888c27be26
freeman <kononov.dmitriy@gmail.com>
2014-03-27 15:50:18 +0600

    visual studio sln added to examples

---
+++ dotnet/href_parser/vs_sln/href_parser/Program.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace href_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // load snippet
+            HtmlDocument htmlSnippet = new HtmlDocument();
+            htmlSnippet = LoadHtmlSnippetFromFile();
+
+            // extract hrefs
+            List<string> hrefTags = new List<string>();
+            hrefTags = ExtractAllAHrefTags(htmlSnippet);
+            Console.WriteLine("Href count = " + hrefTags.Count);
+            foreach (string href in hrefTags)
+            {
+                Console.WriteLine(href);
+            }
+
+        }
+        private static HtmlDocument LoadHtmlSnippetFromFile()
+        {
+            TextReader reader = File.OpenText("HtmlSnippet.txt");
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(reader);
+
+            reader.Close();
+
+            return doc;
+        }
+
+        private static List<string> ExtractAllAHrefTags(HtmlDocument htmlSnippet)
+        {
+            List<string> hrefTags = new List<string>();
+
+            foreach (HtmlNode link in htmlSnippet.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                hrefTags.Add(att.Value);
+            }
+
+            return hrefTags;
+        }
+
+    }
+}
