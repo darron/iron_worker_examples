d5e620ee455bab62d47814ccd49daf922e318ca9
Dmitri Zaitsev <dmitri14@gmail.com>
2014-05-01 07:04:18 +0100

    Added more comments about actions
    Just to sweeten developer's life :)

--- php/hello_worker/enqueue.php
+++ php/hello_worker/enqueue.php
@@ -3,9 +3,11 @@ require_once "phar://../iron_worker.phar";
 
 $worker = new IronWorker();
 
+echo "Starting a task for 'HelloWorker' on Iron.io ... stay tuned ...\n";
+
 $task_id = $worker->postTask('HelloWorker', array(
     'some_param'  => 'some_value',
     'other_param' => array(1, 2, 3)
 ));
 
-echo "Your task #$task_id has been queued up, check https://hud.iron.io to see your task status and log.";
+echo "Your task #$task_id has been queued up, check https://hud.iron.io to see your task status and log.\n";
--- php/hello_worker/upload.php
+++ php/hello_worker/upload.php
@@ -3,4 +3,8 @@ require_once "phar://../iron_worker.phar";
 
 $worker = new IronWorker();
 
+echo "Uploading 'hello_worker.php' to Iron.io as worker named 'HelloWorker' ... stay tuned ...\n";
+
 $worker->upload(dirname(__FILE__), 'hello_worker.php', 'HelloWorker');
+
+echo "... and the upload is finished, now run `php enqueue.php` to run it on Iron.io\n";
